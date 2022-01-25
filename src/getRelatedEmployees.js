4d0bfbe8eea396a2ab41ec6c6c49f434c0fd4255
phvleite <phvleite@gmail.com>
2022-01-25 19:44:28 -0300

    iniciando requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
+const bdEmployees = data.employees;
+// const bdEmployeesManagers =
+
+bdEmployees.forEach((employee) => {
+  employee.managers.forEach((manager) => {
+    console.log(manager);
+  });
+});
+
+// console.log(bdEmployees);
+/*
 function isManager(id) {
   // seu código aqui
+  return bdEmployees.forEach((employee) => {
+
+  });
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const manager = isManager(managerId);
+  try {
+
+  }
+  catch(err) {
+
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
+*/
