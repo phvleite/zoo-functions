17678dea8dffe5ae7cb070b1c6bbced6a22165c9
phvleite <phvleite@gmail.com>
2022-01-25 18:59:10 -0300

    finalizado requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const bdEmployees = data.employees;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let getEmployee = {};
+  bdEmployees.forEach((employee) => {
+    if (employee.firstName === employeeName || employee.lastName === employeeName) {
+      getEmployee = employee;
+    }
+  });
+  return getEmployee;
 }
 
 module.exports = getEmployeeByName;
