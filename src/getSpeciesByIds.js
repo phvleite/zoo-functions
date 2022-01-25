08d48939be6c40aea7c93936b7ec6f7b9eb8935e
phvleite <phvleite@gmail.com>
2022-01-25 15:57:04 -0300

    requisito 01 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const bdSpecies = data.species;
+
+function getSpeciesByIds(ids, ...rest) {
   // seu código aqui
-  
+  const obj = [ids, ...rest];
+  if (obj[0] === undefined) {
+    return [];
+  }
+  const species = [];
+  obj.forEach((getId) => {
+    bdSpecies.forEach((specie) => {
+      if (specie.id === getId) {
+        species.push(specie);
+      }
+    });
+  });
+  return species;
 }
 
 module.exports = getSpeciesByIds;
