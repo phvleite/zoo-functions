8fa1dd17a96036d3115915d07ff7492e21a9a6bf
phvleite <phvleite@gmail.com>
2022-01-25 18:12:56 -0300

    requisito 02 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const bdSpecies = data.species;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const ageResident = [];
+  bdSpecies.forEach((species) => {
+    if (species.name === animal && species.residents.length > 0) {
+      species.residents.forEach((resident) => {
+        ageResident.push(resident.age);
+      });
+    }
+  });
+  return ageResident.every((elem) => elem >= age);
 }
 
 module.exports = getAnimalsOlderThan;
