b5bde6ef25fdef5772da61e82ebb625f70f84c9b
phvleite <phvleite@gmail.com>
2022-01-26 15:49:35 -0300

    transformando em comentário varáveis sem uso

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,14 +1,14 @@
-const en = require('faker/lib/locales/en');
+// const en = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
 
-const dbEntrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
+// const dbEntrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
 const verifyAgeGroup = ((age) => {
   if (age < 18) {
