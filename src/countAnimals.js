ff8323e9506603944cbe90eb5495901390341479
phvleite <phvleite@gmail.com>
2022-01-26 11:30:43 -0300

    finalizado requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,53 @@
 const data = require('../data/zoo_data');
 
+const dbSpecies = data.species;
+
+// dbSpecies.forEach((specie) => console.log(specie));
+
+const countAllAnimals = (() => {
+  const countSpecies = {};
+  dbSpecies.forEach((specie) => {
+    countSpecies[`${specie.name}`] = specie.residents.length;
+  });
+  return countSpecies;
+});
+
+const countSpecie = ((animal) => {
+  let qtdSpecie = 0;
+  dbSpecies.forEach((specie) => {
+    if (specie.name === animal) {
+      qtdSpecie = specie.residents.length;
+    }
+  });
+  return qtdSpecie;
+});
+
+const countSpecieSex = ((animal, sex) => {
+  let qtdSpecieSex = 0;
+  dbSpecies.forEach((specie) => {
+    if (specie.name === animal) {
+      specie.residents.forEach((resident) => {
+        if (resident.sex === sex) {
+          qtdSpecieSex += 1;
+        }
+      });
+    }
+  });
+  return qtdSpecieSex;
+});
+
 function countAnimals(animal) {
   // seu código aqui
+  if (typeof animal === 'undefined') {
+    const countSpecies = countAllAnimals();
+    return countSpecies;
+  }
+  if (Object.keys(animal).length === 1 && Object.keys(animal)[0] === 'specie') {
+    const qtdSpecie = countSpecie(animal.specie);
+    return qtdSpecie;
+  }
+  const qtdSpecieSex = countSpecieSex(animal.specie, animal.sex);
+  return qtdSpecieSex;
 }
 
 module.exports = countAnimals;
