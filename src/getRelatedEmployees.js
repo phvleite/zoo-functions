a33a5e6d5f4a6684f5e237a88e96ea1b600d5aef
phvleite <phvleite@gmail.com>
2022-01-25 23:30:11 -0300

    finalizado requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,33 +1,67 @@
 const data = require('../data/zoo_data');
 
-const bdEmployees = data.employees;
-// const bdEmployeesManagers =
+const dbEmployees = data.employees;
 
-bdEmployees.forEach((employee) => {
-  employee.managers.forEach((manager) => {
-    console.log(manager);
+const getManagers = (() => {
+  const dbManagers = [];
+  dbEmployees.forEach((employee) => {
+    employee.managers.forEach((manager) => {
+      dbManagers.push(manager);
+    });
   });
+  const newdbManagers = dbManagers.filter((elem, ind) => dbManagers.indexOf(elem) === ind);
+  return newdbManagers;
 });
 
-// console.log(bdEmployees);
-/*
-function isManager(id) {
-  // seu código aqui
-  return bdEmployees.forEach((employee) => {
+const dbManagers = getManagers();
 
+const getTeam = (() => {
+  const dbTeam = [];
+  dbEmployees.forEach((employee) => {
+    const { id, managers, firstName, lastName } = employee;
+    dbTeam.push({ id, managers, firstName, lastName });
   });
+  const dbManagersTeam = [];
+  dbManagers.forEach((manager) => {
+    dbTeam.forEach((employee) => {
+      employee.managers.forEach((response) => {
+        if (manager === response) {
+          dbManagersTeam.push([manager, [employee.id, employee.firstName, employee.lastName]]);
+        }
+      });
+    });
+  });
+  return dbManagersTeam;
+});
+
+const dbTeam = getTeam();
+
+function isManager(id) {
+  let result = false;
+  dbManagers.forEach((manager) => {
+    if (manager === id) {
+      result = true;
+    }
+  });
+  return result;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
-  const manager = isManager(managerId);
   try {
-
-  }
-  catch(err) {
-
+    if (!isManager(managerId)) {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    }
+    const dbManagerTeam = [];
+    dbTeam.forEach((id, idx, arr) => {
+      if (managerId === arr[idx][0]) {
+        dbManagerTeam.push(`${arr[idx][1][1]} ${arr[idx][1][2]}`);
+      }
+    });
+    return dbManagerTeam;
+  } catch (err) {
+    throw err.message;
   }
 }
 
 module.exports = { isManager, getRelatedEmployees };
-*/
